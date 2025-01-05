32a9dbc17bde7b93915144ddd638edeb0d5e12e9
Nozaki Rikuto <s23C1106HE@s.chibakoudai.jp>
2025-01-05 22:40:16 +0900

    confirmation

--- test.bash
+++ test.bash
@@ -5,25 +5,30 @@
 ng () {
 	echo ${1}行目が違う
 	res = 1
-        echo ${1}行目が違う
-        res = 1
 }
 res=0
 
 # 有効な数字
 out=$( echo 45 | ./functions)
-[ "{out}" = $("角度: 45.0度\nsin(45.0) = 0.70711\ncos(45.0) = 0.70711\ntan(45.0) = 1.0\n") || ng "$LINENO"
+get="sin(45) = 0.70701
+     cos(45) = 0.70711
+     tan(45) = 1.0"
+[ "${out}" = "${get}" ] || ng "$LINENO"
 
-# 数字以外の入力
+# 異常な入力
+
 out=$( echo www | ./functions)
-[ "{out}" = $("数字ではないものが入力されています") || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-# 空入力
 out=$( echo "" | ./functions)
-[ "{out}" = $("数字ではないものが入力されています") || ng "$LINENO"　 
-=======
-[ "{out}" = $("数字ではないものが入力されています") || ng "$LINENO"
+[ "$?"  = 1 ]     || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$( echo 45? | ./functions)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 # テスト結果
-[ "${res}" = 0 ] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
